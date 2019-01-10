b15c71e5ba6c57541e8d83a703a976f60f0749ff
Hans Poo <hans@welinux.cl>
2019-01-10 17:17:31 -0300

    Luego del build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-
 import promiseMiddleware from 'redux-promise-middleware';
+import errorMiddleware from './errorMiddleware';
+
 import adultosReducer from './adultosReducer';
 
 const rootReducer = combineReducers({
   adultosReducer
 });
 
-const composeStoreWithMiddleware = applyMiddleware(promiseMiddleware())(createStore);
+const composeStoreWithMiddleware = applyMiddleware(promiseMiddleware(), errorMiddleware)(
+  createStore
+);
 
 export default composeStoreWithMiddleware(rootReducer);
